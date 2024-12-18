62225d50604b98d9a948233bb1ca86635761509f
Mor Boneh <mormor318@gmail.com>
2024-12-18 15:17:35 +0200

    changed the manifest in a way that i defined the HomeActivity to be the main page of the app. MainActiviry will still manage the game. also changed the xml file of the score board to add buttons to the layout and added them to the score board java code with listeners so you could retry the game or go back to the home page.

--- app/src/main/java/com/example/trivia/ScoreBoard.java
+++ app/src/main/java/com/example/trivia/ScoreBoard.java
@@ -1,8 +1,10 @@
 package com.example.trivia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoreBoard extends AppCompatActivity {
@@ -13,10 +15,29 @@ public class ScoreBoard extends AppCompatActivity {
         setContentView(R.layout.score_board);
 
         TextView scoreTextView = findViewById(R.id.score_text);
+        Button retryButton = findViewById(R.id.retry_button);
+        Button homeButton = findViewById(R.id.home_button);
 
         int score = getIntent().getIntExtra("SCORE", 0);
         int totalQuestions = getIntent().getIntExtra("TOTAL_QUESTIONS", 0);
-
         scoreTextView.setText("Your score: " + score + "/" + totalQuestions);
+
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreBoard.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreBoard.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
